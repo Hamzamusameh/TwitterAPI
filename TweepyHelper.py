500e51ce2d667d7178b97f80fa4af014b9da3ede
Hamza Musameh <hamza.musameh@hotmail.com>
2019-11-24 22:25:47 +0200

    init commit

---
+++ TweepyHelper.py
@@ -0,0 +1,22 @@
+import tweepy as tp
+
+
+class TweepyHelper:
+    def __init__(self, consumerKey, consumerSecret, accessToken, accessTokenSecret, tweetBody):
+        self.consumerKey = consumerKey
+        self.consumerSecret = consumerSecret
+        self.accessToken = accessToken
+        self.accessTokenSecret = accessTokenSecret
+        self.tweetBody = tweetBody
+
+    def PostTweet(self):
+        try:
+            auth = tp.OAuthHandler(self.consumerKey, self.consumerSecret)
+            auth.set_access_token(self.accessToken, self.accessTokenSecret)
+            api = tp.API(auth)
+            api.update_status(self.tweetBody)
+            print('A tweet is posted')
+            return True
+        except Exception as e:
+            print(e, ' try again..')
+            return False
